43e8b243fce3afd9f52b24c328c6684c81aa5b41
Milben Tan <milben.tan@milbentkingwave.lan>
2023-04-02 16:27:25 -0400

    contract 1

--- examples/test/Example1.js
+++ examples/test/Example1.js
@@ -1,5 +1,36 @@
 const {expect} = require('chai');
 
 describe('Contract: BondExample1', function(){
+    let BondToken;
+    let bondToken;
+    let owner;
+    
+    const ISSUERNAME="American Honda Company";
+    const ISSUERSYMBOL="US02665WDN83";
+    
+    beforeEach(async function(){
+        BondToken = await ethers.getContractFactory("Example1");
+        [owner] = await ethers.getSigners();
+
+        bondToken= await BondToken.deploy(ISSUERNAME, ISSUERSYMBOL, 18, ethers.utils.parseEther("2000"));
+    })
+    
+    describe("Deployment", function(){
+        it("should set the correct name", async function() {
+            expect(await bondToken.name()).to.equal(ISSUERNAME);
+        });
+        it("should set the correct symbol", async function() {
+            expect(await bondToken.symbol()).to.equal(ISSUERSYMBOL);
+        });
+        it("should set the correct decimals", async function() {
+            expect(await bondToken.decimals()).to.equal(18);
+        });
+        it("should set the correct total supply", async function() {
+            expect(await bondToken.totalSupply()).to.equal(ethers.utils.parseEther("2000"));
+        });
+        it("should assign the total Supply to the owner", async function(){
+            expect(await bondToken.balanceOf(owner.address)).to.equal(ethers.utils.parseEther("2000"))
+        })
+    });
 
 });
